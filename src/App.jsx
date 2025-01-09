5754ccff08b3d918eeec6b2c7813147c8deab132
Yuslash <sibikrishna71@gmail.com>
2025-01-09 15:47:00 +0530

    installed three react three fiber in this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { Canvas } from '@react-three/fiber'
 import './index.css'
-
 
 export default function App() {
 
-  return <>
-    <h1 className='text-black'>how are you guys</h1>
-  </>
+  return <div className='canvas-container w-full h-full'>
+
+    <Canvas style={{ width: '100%', height: '100%', }} >
+
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 2]} />
+
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+
+      </mesh>
+
+    </Canvas>
+
+  </div>
 
 }
 

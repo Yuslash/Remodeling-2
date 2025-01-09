6b6f89cb98b0775b2480c89f519783cb1a65f26b
Yuslash <sibikrishna71@gmail.com>
2025-01-10 03:59:30 +0530

    created a rough structure on canvas for three js

--- src/Components/SubHomeBody/MainBodyCanvas.jsx
+++ src/Components/SubHomeBody/MainBodyCanvas.jsx
@@ -1,7 +1,30 @@
+import { OrbitControls, Text } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 export default function MainBodyCanvas() {
   return <>
-    <div className="w-full h-full bg-blue-500 flex justify-center items-center">
-      <div>This is Canvas</div>
+    <div className="w-[40%] h-full bg-blue-500 flex justify-center items-center">
+      <Canvas
+        style={{width: '100%', height: '100%'}}
+        camera={{position: [0,0,6]}}
+      >
+        <mesh>
+          <planeGeometry args={[3,5]}/>
+          <meshNormalMaterial/>
+        </mesh>
+
+     <Text
+        position={[0, 3, 0]} // Position above the box
+        fontSize={0.5}          // Adjust font size
+        color="white"           // Text color
+        anchorX="center"        // Horizontal alignment
+        anchorY="middle"        // Vertical alignment
+      >
+       Here is your Certificate
+      </Text>
+
+        <OrbitControls />
+      </Canvas>
     </div>
   </>
 }

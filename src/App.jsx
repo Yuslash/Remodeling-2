244abd983ba560c6eb3ed472f5884dd90ba6ff3a
Yuslash <sibikrishna71@gmail.com>
2025-01-12 19:32:21 +0530

    the model has lot of nodes so need to reasearch so switch back to windows

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import Components from "./Components/Components";
+import Experience from "./Remodeling/Experience";
 
 export default function App() {
 
   return <div className="w-full h-full flex flex-col top-0 left-0 absolute">
-  
-    <Components />
-     
+        <Experience />
   </div>
 }

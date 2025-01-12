61c9e6a897b72e1bfa003bd3666360788e324801
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-13 00:29:52 +0530

    in nromal shape we are getting what we excepting now in blender model

--- src/Remodeling/Experience.jsx
+++ src/Remodeling/Experience.jsx
@@ -1,39 +1,32 @@
-import { useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { EffectComposer, Bloom, ToneMapping } from '@react-three/postprocessing'
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Bloom, EffectComposer, ToneMapping } from "@react-three/postprocessing";
+import { useControls } from "leva";
+import { PlaneGeometry } from "three";
 
 export default function Experience() {
- 
-  return (
-    <Canvas flat orthographic camera={{ zoom: 100 }}>
-      <color attach="background" args={['#111']} />
-      <ambientLight />
-      <EffectComposer disableNormalPass>
-        <Bloom mipmapBlur luminanceThreshold={1} levels={levels} intensity={intensity * 4} />
-        <ToneMapping />
-      </EffectComposer>
-      <Shape color="hotpink" position={[-2, 0, 0]}>
-        <planeGeometry args={[1.5, 1.5]} />
-      </Shape>
-      <Shape color="orange" position={[0, -0.25, 0]} rotation={[0, 0, Math.PI / 2]}>
-        <circleGeometry args={[1, 1]} />
-      </Shape>
-      <Shape color="skyblue" position={[2, 0, 0]}>
-        <circleGeometry args={[0.8, 64]} />
-      </Shape>
-    </Canvas>
-  )
+
+  const { levels, intensity } = useControls({
+    intensity: { value: 0.4, min: 0, max: 1.5, step: 0.01 },
+    levels: { value: 8, min: 1, max: 9, step: 1 }
+  })
+
+  return <>
+    <div className="w-full h-full">
+        <Canvas flat orthographic camera={{zoom: 100}}>
+          <ambientLight intensity={0.5} />
+          <EffectComposer disableNormalPass>
+            <Bloom intensity={3} mipmapBlur luminanceThreshold={1} levels={10}/>
+              <ToneMapping />
+          </EffectComposer>
+          <mesh>
+            <planeGeometry args={[2,2]}/>
+            <meshStandardMaterial color='yellow' emissive="yellow" emissiveIntensity={2} />
+          </mesh>
+          <OrbitControls />
+        </Canvas>
+    </div>
+  </>
 }
 
-function Shape({ children, color, ...props }) {
-  const [hovered, hover] = useState(true)
-  return (
-    <mesh {...props} onPointerOver={() => hover(false)} onPointerOut={() => hover(true)}>
-      {children}
-      {/* In order to get selective bloom we must crank colors out of
-        their 0-1 spectrum. We push them way out of range. What previously was [1, 1, 1] now could
-        for instance be [10, 10, 10]. */}
-      <meshStandardMaterial color={color} emissive={color} emissiveIntensity={!hovered ? 4 : 0} />
-    </mesh>
-  )
-}
+// got good animatino idea that try to rotate this plane in orbit controls it gives door openning animatio

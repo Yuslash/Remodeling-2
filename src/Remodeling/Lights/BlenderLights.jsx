e3899a5242105a55d91e91d0169fd98af0044ee4
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-14 18:25:09 +0530

    yeah we have fixed theh error for delay rendering issue

--- src/Remodeling/Lights/BlenderLights.jsx
+++ src/Remodeling/Lights/BlenderLights.jsx
@@ -1,30 +1,43 @@
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useControls } from "leva";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const LightInBlender = () => {
 
-    const { intensity } = useControls({
-        intensity: {value: 10, min: 0.1, max: 100, step: 0.01}
+    const { intensity, xPosition, yPosition, zPosition } = useControls({
+        intensity: {value: 10, min: 0.1, max: 100, step: 0.01},
+        xPosition: {value: -2, min: -5, max: 5, step: 0.001 },
+        yPosition: {value: 1, min: -5, max: 5, step: 0.001 },
+        zPosition: {value: 0, min: -5, max: 5, step: 0.001 }
     })
 
     const { scene, nodes } = useGLTF('/light1.glb')
 
+    const spotlightRef = useRef(null)
+    const suzanneRef = useRef(null)
+
     useEffect(() => {
-        if(nodes.Spot) {
-            nodes.Spot.intensity = intensity
-            nodes.Spot.lookAt(nodes.Suzanne.position)
-        }
+        if (nodes.Spot && nodes.Suzanne) {
+            spotlightRef.current = nodes.Spot
+            suzanneRef.current = nodes.Suzanne
+        
+            spotlightRef.current.intensity = intensity
 
-        if (nodes.Suzanne) {
-            nodes.Suzanne.position.set(-2,1,0)
-        }
-    },[intensity, nodes])
+            suzanneRef.current.position.set(xPosition, yPosition, zPosition);
 
+
+            spotlightRef.current.lookAt(suzanneRef.current.position)
+        }
+    },[intensity, nodes, xPosition, yPosition, zPosition, ])
+
+    if (!nodes.Spot || !nodes.Suzanne) {
+        return null; // Wait until the necessary nodes are available
+    }
 
     return <primitive object={scene}  />
 }
+
 
 export default function BlenderLights() {
     return (

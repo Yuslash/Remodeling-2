ee49bef48968253b475fb705484d6399842bccff
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-14 17:45:00 +0530

    in blender the area light is not worknig so i added the area light manualyy in the three js

--- src/Remodeling/Lights/SpotLight.jsx
+++ src/Remodeling/Lights/SpotLight.jsx
@@ -1,15 +1,39 @@
 import { OrbitControls, useGLTF } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { Canvas, useThree } from "@react-three/fiber"
+import { useEffect } from "react"
+import * as THREE from 'three'
 
 const LightModel = () => {
 
-    const { scene, nodes } = useGLTF("/light1.glb")
+    const {  nodes } = useGLTF("/light1.glb")
 
-    console.log(nodes)
+    const monkey = nodes.Suzanne
+    const plane = nodes.Plane
 
-    return (
-        <primitive object={scene} />
-    )
+      return (
+        <>
+        <primitive object={monkey} />
+        <primitive object={plane} />
+        </>
+      )
+}
+
+const RectangleAreaLight = () => {
+    const {scene} = useThree()
+    
+    useEffect(() => {
+        const rectLight = new THREE.RectAreaLight(0x03fca9, 5, 10, 1)  // (color, intensity, width, height)
+        rectLight.position.set(0,3,3)
+        rectLight.lookAt(0,0,0)
+
+        scene.add(rectLight)
+
+        return () => {
+            scene.remove(rectLight)
+        }
+    },[scene])
+
+    return null
 }
 
 export default function SpotLight() {
@@ -22,6 +46,7 @@ export default function SpotLight() {
             >
                 <ambientLight intensity={0.5} />
                 <LightModel />
+                <RectangleAreaLight />
                 <OrbitControls />
 
             </Canvas>

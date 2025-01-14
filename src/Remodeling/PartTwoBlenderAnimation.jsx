9a9d71e04fa3a4a291cf6c7fe286308ec95dd4fc
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-14 15:29:31 +0530

    yeah the animation is good right now

--- src/Remodeling/PartTwoBlenderAnimation.jsx
+++ src/Remodeling/PartTwoBlenderAnimation.jsx
@@ -1,25 +1,30 @@
-import { useAnimations, useGLTF } from "@react-three/drei"
+import { OrbitControls, useAnimations, useGLTF } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { EffectComposer, Bloom, ToneMapping } from "@react-three/postprocessing"
 import { useEffect, useRef } from "react"
 import * as THREE from 'three'
 
 const Animations = () => {
-  const { scene, animations } = useGLTF('/different.glb')
+
+  const { scene, animations, nodes } = useGLTF('/different.glb')
   const { actions } = useAnimations(animations, scene)
 
   const modelRef = useRef()
 
+  console.log(scene)
+
   useEffect(() => {
     if (modelRef.current) {
       modelRef.current.rotation.y = Math.PI / 2
-      modelRef.current.traverse((child) => {
-        if (child.isMesh && child.material) {
+      
+      modelRef.current.children.forEach((child, index) => {
+        if(index === 0) {
           child.material.color = new THREE.Color(0xff0000)
           child.material.emissive = new THREE.Color(0xff0000)
           child.material.emissiveIntensity = 10
         }
       })
+
     }
     if (actions && Object.keys(actions).length > 0) {
       actions[Object.keys(actions)[0]].play()
@@ -46,12 +51,13 @@ export default function PartTwoBlenderAnimations() {
             intensity={0.4 * 4}
             mipmapBlur
             luminanceThreshold={0.5}
-            levels={8}
+            levels={9}
           />
           <ToneMapping /> 
         </EffectComposer>
         <ambientLight intensity={0.5} />
         <Animations />
+        <OrbitControls />
       </Canvas>
     </div>
   )

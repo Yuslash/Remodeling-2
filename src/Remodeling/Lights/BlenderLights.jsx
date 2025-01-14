4aaf964ad65b5d6e49d8f2130a483de33772ec94
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-14 18:31:04 +0530

    yeah added color picker next is creating the model

--- src/Remodeling/Lights/BlenderLights.jsx
+++ src/Remodeling/Lights/BlenderLights.jsx
@@ -2,14 +2,16 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useControls } from "leva";
 import { useEffect, useRef } from "react";
+import * as THREE from 'three'
 
 const LightInBlender = () => {
 
-    const { intensity, xPosition, yPosition, zPosition } = useControls({
-        intensity: {value: 10, min: 0.1, max: 100, step: 0.01},
+    const { intensity, xPosition, yPosition, zPosition, lightColor } = useControls({
+        intensity: {value: 10, min: 0.1, max: 1000, step: 0.01},
         xPosition: {value: -2, min: -5, max: 5, step: 0.001 },
         yPosition: {value: 1, min: -5, max: 5, step: 0.001 },
-        zPosition: {value: 0, min: -5, max: 5, step: 0.001 }
+        zPosition: {value: 0, min: -5, max: 5, step: 0.001 },
+        lightColor: {value: "#ff0000"}
     })
 
     const { scene, nodes } = useGLTF('/light1.glb')
@@ -23,13 +25,15 @@ const LightInBlender = () => {
             suzanneRef.current = nodes.Suzanne
         
             spotlightRef.current.intensity = intensity
+            spotlightRef.current.color = new THREE.Color(parseInt(lightColor.replace('#', '0x'))); // Convert to 0x format
+
 
             suzanneRef.current.position.set(xPosition, yPosition, zPosition);
 
 
             spotlightRef.current.lookAt(suzanneRef.current.position)
         }
-    },[intensity, nodes, xPosition, yPosition, zPosition, ])
+    },[intensity, nodes, xPosition, yPosition, zPosition, lightColor ])
 
     if (!nodes.Spot || !nodes.Suzanne) {
         return null; // Wait until the necessary nodes are available

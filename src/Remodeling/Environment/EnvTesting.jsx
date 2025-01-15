b3a1dca6d5af4781d19761a56c5d099e4415666d
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-15 17:33:53 +0530

    stilll i having issue with file not found but its already there

--- src/Remodeling/Environment/EnvTesting.jsx
+++ src/Remodeling/Environment/EnvTesting.jsx
@@ -1,20 +1,37 @@
-import { Environment, OrbitControls, useGLTF } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { Environment, Html, OrbitControls, useGLTF, useProgress } from "@react-three/drei"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { Suspense } from "react"
+import { CubeTextureLoader } from "three"
 
-const TestEnv = () => {
+function Loader() {
+    
+    const { progress } = useProgress()
 
-    const { scene, nodes } = useGLTF()
-
+    return (
+        <Html center>
+            <div className="w-full h-full" style={{color: "white"}}>{Math.floor(progress)}%</div>
+        </Html>
+    )
 }
 
 export default function EnvTesting() {
 
+    const cubeTexture = useLoader(CubeTextureLoader, [
+        'map/px.png',
+        'map/nx.png',
+        'map/py.png',
+        'map/ny.png',
+        'map/pz.png',
+        'map/nz.png',
+    ])
+
+
     return (
-        <div className="w-full h-full ">
+        <div className="w-full h-full bg-black">
              <Canvas>
 
              <mesh position={[0, 0, 0]}>
-                    <boxGeometry args={[5, 5, 5]} />
+                    <boxGeometry args={[2, 2, 2]} />
                     <meshStandardMaterial 
                     color="orange" 
                     metalness={1}
@@ -22,9 +39,12 @@ export default function EnvTesting() {
                     />
                 </mesh>
 
+                <Suspense fallback={<Loader />}>
                 <Environment
-                background={true} 
-                preset="city" />
+                background
+                files={cubeTexture}
+                 />
+                </Suspense>
 
                 <OrbitControls />
 

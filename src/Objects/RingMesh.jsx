2f2c16ce544344792b27a2fe3550ba437b0680c9
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-22 14:23:35 +0530

    trouble to animate the thetaStart

--- src/Objects/RingMesh.jsx
+++ src/Objects/RingMesh.jsx
@@ -1,24 +1,33 @@
 import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { useControls } from "leva";
+import { useState } from "react";
 
 export default function RingMesh() {
 
-    const { thetaLength } = useControls({
-        thetaLength: {value: 0, min: 0, max: 7, step: 0.01}
+    const { thetaLength,  } = useControls({
+        thetaLength: {value: 0.5, min: 0, max: 7, step: 0.01},
+        // thetaStart: {value: 5, min: 0, max: 7, step: 0.01},
     })
+
+    const [thetaStart, setThetaStart] = useState(5); // Initial value
+
+    // Animate the thetaStart value
+    useFrame(() => {
+      setThetaStart(prev => (prev + 0.01) % (Math.PI * 2)); // Increase and loop after full rotation
+    });
  
     const outerRadius = 2
     const innerRadius = 1.95
 
     return (
-        <div className="w-full h-full">
+        <div className="w-full h-full bg-black">
             <Canvas>
-                <mesh>
-                    <circleGeometry args={[outerRadius, 64, 0, thetaLength]} />
+                <mesh rotation={[-Math.PI / 2,0,0]} position={[0,-1.01,0]}>
+                    <circleGeometry args={[outerRadius, 64, thetaStart, thetaLength]} />
                     <meshBasicMaterial color="red" />
                 </mesh>
-                <mesh position={[0,0,0.01]}>
+                <mesh rotation={[- Math.PI / 2,0,0]} position={[0,-1,0.01]}>
                     <circleGeometry args={[innerRadius, 64]} />
                     <meshBasicMaterial color="blue" />
                 </mesh>

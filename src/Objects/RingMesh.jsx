733798b147f3204e03de663ba427e77377120df8
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-22 13:16:04 +0530

    instead of using shaders im using two geometry

--- src/Objects/RingMesh.jsx
+++ src/Objects/RingMesh.jsx
@@ -1,14 +1,26 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useControls } from "leva";
 
 export default function RingMesh() {
 
-    return(
-        <div className="w-full h-full ">
+    const { thetaLength } = useControls({
+        thetaLength: {value: 0, min: 0, max: 7, step: 0.01}
+    })
+ 
+    const outerRadius = 2
+    const innerRadius = 1.95
+
+    return (
+        <div className="w-full h-full">
             <Canvas>
                 <mesh>
-                    <ringGeometry />
-                    <meshStandardMaterial />
+                    <circleGeometry args={[outerRadius, 64, 0, thetaLength]} />
+                    <meshBasicMaterial color="red" />
+                </mesh>
+                <mesh position={[0,0,0.01]}>
+                    <circleGeometry args={[innerRadius, 64]} />
+                    <meshBasicMaterial color="blue" />
                 </mesh>
                 <OrbitControls />
             </Canvas>
